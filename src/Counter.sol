49f46fabeaf944da63ef7f5b8e76c3ad0baecb98
zenground0 <ZenGround0@users.noreply.github.com>
2022-11-10 22:21:59 +0000

    hack to read method number

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    uint256 public fallback_set;
+    bytes public fallback_calldata;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
@@ -10,5 +12,18 @@ contract Counter {
 
     function increment() public {
         number++;
+    }
+
+    fallback (bytes calldata input) external payable returns (bytes memory _output) {
+        uint method = readMethodNum();
+        fallback_set = method;
+        fallback_calldata = input;
+    }
+
+    function readMethodNum() private view returns (uint method) {
+        // HACK HACK HACK: we'll sub out difficulty opcode after compiling with the FVM extension
+        assembly {
+            method := difficulty()
+        }
     }
 }
