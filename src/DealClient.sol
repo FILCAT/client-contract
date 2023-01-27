4cb22d1f964266f878ce80f2ca9cd7e2da31b3fb
Jennifer Wang <jiayingw703@gmail.com>
2023-01-26 19:12:14 -0500

    cleanup

--- src/DealClient.sol
+++ src/DealClient.sol
@@ -1,16 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {StdStorage} from "../lib/forge-std/src/Components.sol";
 import {specific_authenticate_message_params_parse, specific_deal_proposal_cbor_parse} from "./CBORParse.sol";
-
-contract MockMarket {
-    function publish_deal(bytes memory raw_auth_params, address callee) public {
-        // calls standard filecoin receiver on message authentication api method number
-        (bool success, ) = callee.call(abi.encodeWithSignature("handle_filecoin_method(uint64,uint64,bytes)", 0, 2643134072, raw_auth_params));
-        require(success, "client contract failed to authorize deal publish");
-    }
-}
 
 contract DealClient {
 
@@ -18,7 +9,7 @@ contract DealClient {
     uint64 constant public DATACAP_RECEIVER_HOOK_METHOD_NUM = 3726118371;
 
     mapping(bytes => bool) public cidSet;
-    mapping(bytes => uint) public cidSizes;
+    //mapping(bytes => uint) public cidSizes;
     mapping(bytes => mapping(bytes => bool)) public cidProviders;
     event ReceivedDataCap(string received);
 
@@ -28,32 +19,32 @@ contract DealClient {
         owner = msg.sender;
     }
 
-    function addCID(bytes calldata cidraw, uint size) public {
+    function addCID(bytes calldata pieceCID, uint size) public {
        require(msg.sender == owner);
-       cidSet[cidraw] = true;
-       cidSizes[cidraw] = size;
+       cidSet[pieceCID] = true;
+       //cidSizes[pieceCID] = size;
     }
 
-    function policyOK(bytes calldata cidraw, bytes calldata provider) internal view returns (bool) {
-        bool alreadyStoring = cidProviders[cidraw][provider];
+    function policyOK(bytes calldata pieceCID, bytes calldata provider) internal view returns (bool) {
+        bool alreadyStoring = cidProviders[pieceCID][provider];
         return !alreadyStoring;
     }
 
-    function authorizeData(bytes calldata cidraw, bytes calldata provider, uint size) public {
+    function authorizeData(bytes calldata pieceCID, bytes calldata provider, uint size) public {
         // if (msg.sender != f05) return;
-        require(cidSet[cidraw], "cid must be added before authorizing");
-        require(cidSizes[cidraw] == size, "data size must match expected");
-        require(policyOK(cidraw, provider), "deal failed policy check: has provider already claimed this cid?");
+        require(cidSet[pieceCID], "cid must be added before authorizing");
+        require(cidSizes[pieceCID] == size, "data size must match expected");
+        require(policyOK(pieceCID, provider), "deal failed policy check: has provider already claimed this cid?");
 
-        cidProviders[cidraw][provider] = true;
+        cidProviders[pieceCID][provider] = true;
     }
 
     function handle_filecoin_method(uint64 method, uint64, bytes calldata params) public {
         // dispatch methods
         if (method == AUTHORIZE_MESSAGE_METHOD_NUM) {
             bytes calldata deal_proposal_cbor_bytes = specific_authenticate_message_params_parse(params);
-            (bytes calldata cidraw, bytes calldata provider, uint size) = specific_deal_proposal_cbor_parse(deal_proposal_cbor_bytes);
-            authorizeData(cidraw, provider, size);
+            (bytes calldata pieceCID, bytes calldata provider, uint size) = specific_deal_proposal_cbor_parse(deal_proposal_cbor_bytes);
+            authorizeData(pieceCID, provider, size);
         } else if (method == DATACAP_RECEIVER_HOOK_METHOD_NUM) {
              emit ReceivedDataCap("DataCap Received!");
         } else {
